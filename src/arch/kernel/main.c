632db1f7a928c39ecca77d1b8736dbe58c5777e3
frodggy <75814033+frodggy@users.noreply.github.com>
2022-03-15 13:21:15 -0400

    updated main.c

--- src/arch/kernel/main.c
+++ src/arch/kernel/main.c
@@ -1,8 +1,8 @@
 #include "./lib/print.c"
+#include "./lib/apps/terminal.c"
+
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
-    print_str("Welcome to Core os powered by the Nexus kernel\n");
-    print_str("Press [ENTER] to continue");
+    nxt::apps::terminal()
 }
